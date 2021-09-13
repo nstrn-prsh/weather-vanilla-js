8a7b25fc439da414e798a35323f003dc611560fb
nastaran poursharif <nastaran.p.sh.77@gmail.com>
2021-09-13 22:16:51 +0430

    7th: regexp

--- src/app.js
+++ src/app.js
@@ -5,12 +5,17 @@ let city = store.getCityInLocaleStorage();
 let weatherData = new Weather(`${store.city}`);
 let ui = new UI();
 
+//event listeners
+
 //baraye inke findWeather() invoke beshe
 document.addEventListener("DOMContentLoaded", () => {
      findWeather();
 });
 //changing the city
+document.querySelector("#city").addEventListener("input", controlCity);
 document.querySelector("#w-change-btn").addEventListener("click", changeCity);
+
+//functions
 
 function findWeather() {
      weatherData
@@ -23,6 +28,22 @@ function findWeather() {
                console.log(error.message);
                localStorage.clear();
           });
+}
+
+//check konim ke toye input meghdare dorost benevise
+function controlCity() {
+     let city = document.querySelector("#city").value;
+     let regexp = /^\b[a-zA-Z]+$/gi;
+     if (regexp.test(city) == false) {
+          let newCityDiv = document.querySelector("#new-city");
+          let errorMsg = document.createElement("span");
+          errorMsg.innerHTML = `<p>write a city name!</p>`;
+          newCityDiv.appendChild(errorMsg);
+          setTimeout(() => {
+               errorMsg.textContent = null;
+          }, 5000);
+          newCityDiv.appendChild(errorMsg);
+     }
 }
 
 //az input city meghdaresho migirim
